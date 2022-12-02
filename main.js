bba98fb8c08d6e7078a81a618556418a0ea89c50
SaiKirtan <104989047+SaiKirtan@users.noreply.github.com>
2022-12-02 17:54:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var fname = "Ranbir";
+var lname = "Singh";
+var fullname = fname.concat(lname);
+
+console.log(fullname);
+
+var a = 5
+var b = 25
+
+var min = Math.min(a, b);
+var max = Math.max(a, b);
+
+console.log(min);
+console.log(max);
